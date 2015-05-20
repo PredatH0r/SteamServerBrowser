2b953e5c86b85ec9e457d2760f0be082f01eac42
hbeham <horst@beham.biz>
2015-05-20 21:05:28 +0200

    added script to create zip package

---
+++ CreateDistribZip.cmd
@@ -0,0 +1,22 @@
+@echo off
+cd /d %~dp0
+set cwd=%cd%
+set curdate=%date:~6,4%-%date:~3,2%-%date:~0,2%
+set target=%cd%\SteamServerBrowser_%curdate%
+set DXversion=14.2
+mkdir "%target%" 2>nul
+del /s /q "%target%\*"
+cd ServerBrowser
+call CodeSigning.cmd
+cd .\bin\debug
+copy ServerBrowser.exe* "%target%"
+copy QueryMaster.dll "%target%"
+copy Ionic.BZip2.dll "%target%"
+copy DevExpress*.dll "%target%"
+cd %cwd%
+copy readme.txt "%target%"
+
+"c:\program files\7-Zip\7z.exe" a -tzip %target%.zip SteamServerBrowser_%curdate%
+
+cd %cwd%
+pause
--- ServerBrowser/CodeSigning.cmd
+++ ServerBrowser/CodeSigning.cmd
@@ -2,7 +2,7 @@
 
 rem -----------------------------
 rem If you want to sigitally sign the generated .exe and .dll files, 
-retm you need to have your code signing certificate installed in the Windows certificate storage
+rem you need to have your code signing certificate installed in the Windows certificate storage
 rem -----------------------------
 
 set olddir=%cd%
@@ -10,7 +10,7 @@ rem disabled for development
 rem goto:eof
 
 set signtool="C:\Program Files\Microsoft SDKs\Windows\v6.0A\Bin\signtool.exe"
-cd /d "%~dp0\bin\Release"
+cd /d "%~dp0\bin\Debug"
 set files=ServerBrowser.exe QueryMaster.dll
 %signtool% sign /a /t "http://timestamp.comodoca.com/authenticode" %files%
 
